2d9710fe13d53248452dc1eae4215f55f13bc65b
tanxiang <1092145181@qq.com>
2024-08-21 22:29:41 +0800

    update color

--- app/Main.tsx
+++ app/Main.tsx
@@ -23,7 +23,7 @@ export default function Home({ posts }) {
           {posts.slice(0, MAX_DISPLAY).map((post) => {
             const { slug, date, title, summary, tags } = post
             return (
-              <li key={slug} className="py-12 hover:bg-gray-50 dark:hover:bg-transparent">
+              <li key={slug} className="py-12 hover:bg-gray-50 dark:hover:bg-zinc-800">
                 <article>
                   <div className="space-y-2 xl:grid xl:grid-cols-4 xl:items-baseline xl:space-y-0">
                     <dl>
